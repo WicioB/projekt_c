5c532dd0a3911c6e404ccf6bc59ce4d348c00f51
WicioB <boczekwiktor@gmail.com>
2026-03-31 11:11:28 +0200

    added prototypes of structures and functions

---
+++ headers/graph.h
@@ -0,0 +1,25 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+typedef struct {
+    int id;
+    double x,y,dx,dy;
+}Vertex;
+
+typedef struct{
+    int x,y;
+    double weight;
+}Edge;
+
+typedef struct{
+    Vertex *vertices;
+    Edge *edges;
+    int number_of_vertices;
+    int number_of_edges;
+} Graph;
+
+Graph* create_graph();
+void free_graph(Graph *g);
+int load_file(Graph *g, char *file);
+int save_file(Graph *g, char *file, char *filetype);
+#endif
